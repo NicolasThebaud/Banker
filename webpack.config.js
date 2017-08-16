f6e1528a4f094a5e700b35fff3c5ecc9da7b2667
NicolasThebaud <thebaud.nicolas012@gmail.com>
2017-08-16 18:35:56 +0200

    Updated webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -16,10 +16,8 @@ module.exports = {
   module: {
     loaders: [
       { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
-      { test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ }
-    ],
-    rules: [
-      { test: /\.scss$/, use: [{ loader: "style-loader" }, { loader: "css-loader" }, { loader: "sass-loader" }]}
+      { test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ },
+      { test: /\.scss$/, loader: 'sass-loader', exclude: /node_modules/ }
     ]
   },
   plugins: [HtmlWebpackPluginConfig]
