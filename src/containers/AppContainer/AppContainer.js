9ff7209660091695226056b7a21d085e90ee171f
NicolasThebaud <thebaud.nicolas012@gmail.com>
2017-08-16 19:25:09 +0200

    Implemented base container boilerplate

---
+++ src/containers/AppContainer/AppContainer.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import history from '../../routes/history'
+
+class AppContainer extends Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    return (
+      <section>
+        HOME
+      </section>
+    )
+  }
+}
+
+export default AppContainer
